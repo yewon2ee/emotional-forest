fac0df28318661947db3e7ace647745cf3031ebd
yewonee <hyewon8685@naver.com>
2025-07-05 22:53:53 +0900

    feature: 토글 생성

--- src/components/common/BottomNav.jsx
+++ src/components/common/BottomNav.jsx
@@ -4,7 +4,9 @@ import { Cog6ToothIcon, HomeIcon, UserIcon} from "@heroicons/react/24/outline";
 
 const BottomNav = () => {
   return (
-    <nav className='wrapper'>
+    // 네비게이션 영역 전체 감싸는 태그 
+    <nav className='wrapper'> 
+        {/* 각 아이콘을 div로 감싸서 css grid or flex 배치쉽게  */}
         <div>
             <Cog6ToothIcon className='setting-icon' />
         </div>
