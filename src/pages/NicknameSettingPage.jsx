99f4928654f77eada2892927ca8e2ccfe2394821
yewon2ee <163722855+yewon2ee@users.noreply.github.com>
2025-07-06 19:03:52 +0900

    Update NicknameSettingPage.jsx

--- src/pages/NicknameSettingPage.jsx
+++ src/pages/NicknameSettingPage.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import Input from "../components/common/Input";
-import Header from '../components/common/Header';ㅍ
+import Header from '../components/common/Header';
 
 
 const NicknameSettingPage = () => {
