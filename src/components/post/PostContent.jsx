639bba1aab24fd6cd5261ffd997052d363cef587
bukyung <boochae2003@gmail.com>
2025-07-08 17:43:03 +0900

    feat: 포스트 디테일 페이지 css 포기 n번째

--- src/components/post/PostContent.jsx
+++ src/components/post/PostContent.jsx
@@ -2,11 +2,10 @@
 import React from 'react';
 //닉네임, 작성 날짜, 내용 
 
-const PostContent = ({nickname,date,content}) => {
+const PostContent = ({nickname,content}) => {
   return (
     <div className = "post-content">
         <p className = "post-nickname">{nickname}</p>
-        <p className = "post-date">{date}</p>
         <p className = "post-content">{content}</p>
     </div>
   )
--- src/components/post/ReactionButtons.jsx
+++ src/components/post/ReactionButtons.jsx
@@ -5,21 +5,34 @@ const ReactionButtons = ({ onLike, onCheer }) => {
     // 각 버튼의 상태를 useState로 관리 
     const [likes, setLikes] = useState(0);
     const [cheers, setCheers] = useState(0);
-  
-    const handleLike = () => {
+
+    const [liked, setLiked] = useState(false);
+    const [cheered, setCheered] = useState(false);
+
+  const handleLike = () => {
+    if (!liked) {
       setLikes(likes + 1);
-      onLike(); // 부모에게도 알림
-    };
-  
-    const handleCheer = () => {
+      onLike?.();
+    } else {
+      setLikes(likes - 1);
+    }
+    setLiked(!liked);
+  };
+
+  const handleCheer = () => {
+    if (!cheered) {
       setCheers(cheers + 1);
-      onCheer(); // 부모에게도 알림
-    };
+      onCheer?.();
+    } else {
+      setCheers(cheers - 1);
+    }
+    setCheered(!cheered);
+  };
   
     return (
       <div className="reaction-buttons">
-        <button onClick={handleLike}>👍 {likes}</button>
-        <button onClick={handleCheer}>💙 {cheers}</button>
+        <button className="like-btn" onClick={handleLike} >LIKE {likes}</button>
+        <button className="cheer-btn" onClick={handleCheer} >CHEER {cheers}</button>
       </div>
     );
   };
