0639c4bfd9c28af37ba704d13a32ea56799be0c8
yewonee <hyewon8685@naver.com>
2025-07-08 20:48:52 +0900

    여기까지 해서 올릴ㄹ게 ...

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://emotioncrossing.p-e.kr/",  //여기를 실제로 배포된 백엔드 주소로 변경하면 됨
+  baseURL: "http://emotioncrossing.p-e.kr",  //여기를 실제로 배포된 백엔드 주소로 변경하면 됨
   headers: {
     "Content-Type": "application/json",
   },
--- src/components/post/ReactionButtons.jsx
+++ src/components/post/ReactionButtons.jsx
@@ -1,40 +1,13 @@
-// 좋아요 / cheer 리액션 버튼
-import React, {useState} from 'react';
+import React from 'react';
+import { FaThumbsUp, FaRegSmile } from 'react-icons/fa';
 
 const ReactionButtons = ({ onLike, onCheer }) => {
-    // 각 버튼의 상태를 useState로 관리 
-    const [likes, setLikes] = useState(0);
-    const [cheers, setCheers] = useState(0);
+  return (
+    <div className="reaction-buttons">
+      <button onClick={onLike}><FaThumbsUp /> LIKE</button>
+      <button onClick={onCheer}><FaRegSmile /> CHEER</button>
+    </div>
+  );
+};
 
-    const [liked, setLiked] = useState(false);
-    const [cheered, setCheered] = useState(false);
-
-  const handleLike = () => {
-    if (!liked) {
-      setLikes(likes + 1);
-      onLike?.();
-    } else {
-      setLikes(likes - 1);
-    }
-    setLiked(!liked);
-  };
-
-  const handleCheer = () => {
-    if (!cheered) {
-      setCheers(cheers + 1);
-      onCheer?.();
-    } else {
-      setCheers(cheers - 1);
-    }
-    setCheered(!cheered);
-  };
-  
-    return (
-      <div className="reaction-buttons">
-        <button className="like-btn" onClick={handleLike} >LIKE {likes}</button>
-        <button className="cheer-btn" onClick={handleCheer} >CHEER {cheers}</button>
-      </div>
-    );
-  };
-  
-  export default ReactionButtons;
+export default ReactionButtons;
